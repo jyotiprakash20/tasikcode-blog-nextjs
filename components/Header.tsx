1414ef9765789c305465bef8571198a93592d066
daguswijaya <dagus.wijaya@gmail.com>
2024-10-04 09:17:23 +0700

    show logo type in small screen

--- components/Header.tsx
+++ components/Header.tsx
@@ -16,9 +16,7 @@ const Header = () => {
       <Link href="/" aria-label={siteMetadata.headerTitle}>
         <div className="flex items-center justify-between">
           {typeof siteMetadata.headerTitle === 'string' ? (
-            <div className="hidden h-6 text-4xl font-semibold sm:block">
-              {siteMetadata.headerTitle}
-            </div>
+            <div className="h-6 text-4xl font-semibold sm:block">{siteMetadata.headerTitle}</div>
           ) : (
             siteMetadata.headerTitle
           )}
